c59898bd86d0221406ba3a37ce0548a82fb4cb2f
16017004 <16017004@L815WD220028.edu.cbe.ab.ca>
2022-09-13 14:30:00 -0600

    2:30pm

---
+++ Chapter 2/src/CRT.java
@@ -0,0 +1,27 @@
+
+public class CRT 
+{
+
+	public static void main(String[] args)
+	{
+		
+		/*
+		 3.
+		 	TestAvg.java
+		 	Calculates test averages.
+		 	Li 
+		 	September 13, 2022
+		 	
+		 4. //The AvgScore displays the mean of a set of scores.
+		 	
+		 5.
+		 	
+		 
+		 6.
+		 7.
+		 12.
+		 */
+
+	}
+
+}
