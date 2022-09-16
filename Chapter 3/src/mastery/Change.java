c2db8ea6b19d184a947232dfbd840d94af6393d6
16017004 <16017004@L815WD220028.edu.cbe.ab.ca>
2022-09-16 11:48:03 -0600

    11:47am

---
+++ Chapter 3/src/mastery/Change.java
@@ -0,0 +1,26 @@
+package mastery;
+/*
+
+Program: Change.java          Last Date of this Revision: September 19, 2022
+
+Purpose: Prompt for an amount less than $1.00 and display the minimum number of coins needed to make the change.
+
+Author: Li Tan, 
+School: CHHS
+Course: Computer Programming 20
+
+*/
+public class Change
+{
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
+/* Screen dump
+
+
+*/
